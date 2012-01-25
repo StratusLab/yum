f9d13ed779298a537007d2fa59a5bec587b5c9a5
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2012-01-25 16:49:44 +0100

    commented out substitution of snapshot version with 0.0

--- src/scripts/pre-build-certification.sh
+++ src/scripts/pre-build-certification.sh
@@ -7,8 +7,8 @@ git checkout ${GIT_BRANCH}
 git pull
 
 # Replace SNAPSHOT versions.
-sed 's%<version>.*-SNAPSHOT</version>%<version>0.0</version>%' pom.xml > pom2.xml
-mv -f pom2.xml pom.xml
+#sed 's%<version>.*-SNAPSHOT</version>%<version>0.0</version>%' pom.xml > pom2.xml
+#mv -f pom2.xml pom.xml
 
 # Update to use latest defined versions.
 mvn versions:use-latest-releases -DgenerateBackupPoms=false
